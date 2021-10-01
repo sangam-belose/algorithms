10a35dce9ed418ebe5634be23a52a96a734005f0
Sangam Belose <sangam.belose@morningstar.com>
2021-10-01 18:31:51 +0530

    recursion for GCD

--- src/com/belose/GCD.java
+++ src/com/belose/GCD.java
@@ -10,14 +10,25 @@ class GCD {
     return num1;
   }
 
+  public static int GCDusingRecursion(int num1, int num2){
+    if (num2 == 0) return num1;
+    int reminder = num1 % num2;
+    return GCDusingRecursion(num2, reminder);
+  }
 
   public static void main(String args[]) {
 
     System.out.println("Finding GCD of two numbers");
-    System.out.println(GCD.getGreatedCommonDivisor(96, 12));
-    System.out.println(GCD.getGreatedCommonDivisor(20, 8));
-    System.out.println(GCD.getGreatedCommonDivisor(50, 15));
-    System.out.println(GCD.getGreatedCommonDivisor(20, 2));
+    System.out.println("GCD of 12 and 96 = "+GCD.getGreatedCommonDivisor(12, 96));
+    System.out.println("GCD of 20 and 8 = "+GCD.getGreatedCommonDivisor(20, 8));
+    System.out.println("GCD of 50 and 15 = "+GCD.getGreatedCommonDivisor(50, 15));
+    System.out.println("GCD of 20 and 2 = "+GCD.getGreatedCommonDivisor(20, 2));
+
+    System.out.println("Finding GCD of two numbers using recursion");
+    System.out.println("GCD of 12 and 96 = "+GCD.GCDusingRecursion(12, 96));
+    System.out.println("GCD of 20 and 8 = "+GCD.GCDusingRecursion(20, 8));
+    System.out.println("GCD of 50 and 15 = "+GCD.GCDusingRecursion(50, 15));
+    System.out.println("GCD of 20 and 2 = "+GCD.GCDusingRecursion(20, 2));
   }
 
 }
